dfe8e4c9d16eed378c3e01c737f5a2e964f48e9b
Brian Chaparro <86704774+Brian-ChM@users.noreply.github.com>
2024-08-27 23:27:12 +0000

    Add hours component

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,19 @@
 // Placeholder to host a component
+const Hours = () => {
+
+    const currentDay = new Date().getDay();
+
+    const hours = {
+        weekday: "10 a.m. - 4 p.m.",
+        weekend: "9 a.m. - 8 p.m."
+    }
+
+    const isWeekend = currentDay == 0 || currentDay == 6;
+    const todayHours = isWeekend ? hours.weekend : hours.weekday;
+
+    return (
+        <p>Today's hours: {todayHours}</p>
+    )
+}
+
+export default Hours;
